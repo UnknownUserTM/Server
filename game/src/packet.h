7b34b471954d884a92cb514ab59a02669ea735af
ImperatorExterminatus <54247646+ImperatorExterminatus@users.noreply.github.com>
2020-12-27 11:23:49 +0100

    Revert "slot system für special inventory"
    This reverts commit 886a48af60addf8a2148a2d7d1fd1928744c00c1.

--- game/src/packet.h
+++ game/src/packet.h
@@ -1365,29 +1365,14 @@ typedef struct packet_shop	// 가변 패킷
 	BYTE        subheader;
 } TPacketGCShop;
 
-// struct packet_exchange
-// {
-	// BYTE	header;
-	// BYTE	sub_header;
-	// BYTE	is_me;
-	// int64_t	arg1;	// vnum
-	// TItemPos	arg2;	// cell
-	// DWORD	arg3;	// count
-	// long	alSockets[ITEM_SOCKET_MAX_NUM];
-	// TPlayerItemAttribute aAttr[ITEM_ATTRIBUTE_MAX_NUM];
-// };
-
 struct packet_exchange
 {
 	BYTE	header;
 	BYTE	sub_header;
 	BYTE	is_me;
-	DWORD	arg1;	// vnum
+	int64_t	arg1;	// vnum
 	TItemPos	arg2;	// cell
 	DWORD	arg3;	// count
-#ifdef WJ_ENABLE_TRADABLE_ICON
-	TItemPos	arg4;	// srccell
-#endif
 	long	alSockets[ITEM_SOCKET_MAX_NUM];
 	TPlayerItemAttribute aAttr[ITEM_ATTRIBUTE_MAX_NUM];
 };
